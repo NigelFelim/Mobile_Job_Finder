25e66f3040c6886024f101c40efb84d6a3ca82f3
Acauu <inggrid.fortuna@student.umn.ac.id>
2021-05-15 02:43:56 +0800

    API Maps

--- app/src/main/java/com/map/mobile_job_finder/MainActivity.java
+++ app/src/main/java/com/map/mobile_job_finder/MainActivity.java
@@ -1,19 +1,18 @@
 package com.map.mobile_job_finder;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/map/mobile_job_finder/MapsActivity.java
+++ app/src/main/java/com/map/mobile_job_finder/MapsActivity.java
@@ -1,42 +1,194 @@
 package com.map.mobile_job_finder;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private final String TAG = getClass().getSimpleName();
+    private String[] places;
+    private LocationManager locationManager;
+    private Location loc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        currentLocation();
+
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+
     }
+
+    private class GetPlaces extends AsyncTask<Void, Void, ArrayList<Place>> {
+
+        private ProgressDialog dialog;
+        private Context context;
+        private String places;
+
+        public GetPlaces(Context context, String places) {
+            this.context = context;
+            this.places = places;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Place> result) {
+            super.onPostExecute(result);
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+            for (int i = 0; i < result.size(); i++) {
+                mMap.addMarker(new MarkerOptions()
+                        .title(result.get(i).getName())
+                        .position(
+                                new LatLng(result.get(i).getLatitude(), result
+                                        .get(i).getLongitude()))
+                        .icon(BitmapDescriptorFactory
+                                .fromResource(R.drawable.background_color))
+                        .snippet(result.get(i).getVicinity()));
+            }
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(new LatLng(result.get(0).getLatitude(), result
+                            .get(0).getLongitude())) // Sets the center of the map to
+                    // Mountain View
+                    .zoom(14) // Sets the zoom
+                    .tilt(30) // Sets the tilt of the camera to 30 degrees
+                    .build(); // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory
+                    .newCameraPosition(cameraPosition));
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(context);
+            dialog.setCancelable(false);
+            dialog.setMessage("Loading..");
+            dialog.isIndeterminate();
+            dialog.show();
+        }
+
+        @Override
+        protected ArrayList<Place> doInBackground(Void... arg0) {
+            PlacesService service = new PlacesService(
+                    "Put your project browser API key here");
+            ArrayList<Place> findPlaces = service.findPlaces(loc.getLatitude(), // 28.632808
+                    loc.getLongitude(), places); // 77.218276
+
+            for (int i = 0; i < findPlaces.size(); i++) {
+
+                Place placeDetail = findPlaces.get(i);
+                Log.e(TAG, "places : " + placeDetail.getName());
+            }
+            return findPlaces;
+        }
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    private void currentLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        String provider = locationManager
+                .getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location == null) {
+            locationManager.requestLocationUpdates(provider, 0, 0, listener);
+        } else {
+            loc = location;
+            new GetPlaces(MapsActivity.this, places[0].toLowerCase().replace(
+                    "-", "_")).execute();
+            Log.e(TAG, "location : " + location);
+        }
+
+    }
+
+    private LocationListener listener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.e(TAG, "location update : " + location);
+            loc = location;
+            locationManager.removeUpdates(listener);
+        }
+    };
+
+
     public void onSearch(View v){
         switch (v.getId()){
             case R.id.btn_search:
@@ -87,8 +239,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng umn = new LatLng(-6.255986, 106.618493);
+        mMap.addMarker(new MarkerOptions().position(umn).title("UMN Lobby"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(umn));
     }
 }
